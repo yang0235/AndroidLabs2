7aff654d88ccf67cbdcd853c42dd21ce221a5a0a
bruce_b_yang@yahoo.com <bruce_b_yang@yahoo.com>
2017-10-13 19:40:11 -0400

    Lab5 finished

---
+++ app/src/main/java/com/example/bruce/androidlabs/ChatDatabaseHelper.java
@@ -0,0 +1,63 @@
+package com.example.bruce.androidlabs;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by bruce on 2017-10-09.
+ */
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+
+    public static String DATABASE_NAME = "Message.db";
+    public static String TABLE_NAME = "message_table";
+    public static int VERSION_NUM = 101;
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_MESSAGE = "message";
+
+    public static String[] MESSAGE_FIELDS = new String[]{
+            KEY_ID,
+            KEY_MESSAGE,
+    };
+
+    private static String CREATE_TABLE_MESSAGE =
+            "CREATE TABLE " + TABLE_NAME + "( "
+                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + KEY_MESSAGE + " text);";
+
+    public ChatDatabaseHelper(Context ctx){
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_MESSAGE);
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
+        Log.w(TAG, "Upgrading database from version " + oldVer + " to " + newVer + ", which will destroy all old data,");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL(CREATE_TABLE_MESSAGE);
+        Log.i("ChatDatabaseHelper","Calling onUpgrade, oldVersion=" + oldVer + "newVersion=" +newVer);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVer, int newVer) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL(CREATE_TABLE_MESSAGE);
+        Log.i("ChatDatabaseHelper","Calling downUpgrade, oldVersion=" + oldVer + "newVersion=" +newVer);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        Log.i("Database ", "onOpen was called");
+    }
+}
--- app/src/main/java/com/example/bruce/androidlabs/ChatWindow.java
+++ app/src/main/java/com/example/bruce/androidlabs/ChatWindow.java
@@ -1,8 +1,15 @@
 package com.example.bruce.androidlabs;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabaseCorruptException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +17,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import static android.os.Build.ID;
+import static com.example.bruce.androidlabs.ChatDatabaseHelper.KEY_ID;
+import static com.example.bruce.androidlabs.ChatDatabaseHelper.KEY_MESSAGE;
+import static com.example.bruce.androidlabs.ChatDatabaseHelper.MESSAGE_FIELDS;
+import static com.example.bruce.androidlabs.ChatDatabaseHelper.TABLE_NAME;
 
 public class ChatWindow extends Activity {
 
@@ -20,6 +34,11 @@ public class ChatWindow extends Activity {
     EditText chatText;
     ListView chatView;
     ArrayList<String>  storedMessage;
+    SQLiteDatabase db;
+    protected static final String ACTIVITY_NAME = "ChatWindow";
+
+//    ChatDatabaseHelper cDbhelper= new ChatDatabaseHelper(this);
+//    final SQLiteDatabase db = cDbhelper.getWritableDatabase();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +52,40 @@ public class ChatWindow extends Activity {
 
         final ChatAdapter messageAdapter = new ChatAdapter(this);
         chatView.setAdapter(messageAdapter);
+        ChatDatabaseHelper cDbhelper= new ChatDatabaseHelper(this);
+        db = cDbhelper.getWritableDatabase();
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                storedMessage.add(chatText.getText().toString());
-                messageAdapter.notifyDataSetChanged();
-                chatText.setText("");
-            }}
-        );
+            String input = chatText.getText().toString();
+            storedMessage.add(input);
+            messageAdapter.notifyDataSetChanged();
+            chatText.setText("");
+
+            ContentValues initial = new ContentValues();
+            initial.put(KEY_MESSAGE,input);
+            db.insert(TABLE_NAME, "", initial);
+        }});
+
+        Cursor cursor = db.query( false, TABLE_NAME, MESSAGE_FIELDS, null, null, null, null, null, null);
+//        Cursor cursor = db.rawQuery("select * from " + TABLE_NAME ,null);
+        int numResults = cursor.getCount();
+        int numColumns = cursor.getColumnCount();
+
+        cursor.moveToFirst();//resets the iteration of results
+        while(!cursor.isAfterLast()) {
+            Log.i(ACTIVITY_NAME, "SQL MESSAGE: " + cursor.getString(cursor.getColumnIndex(KEY_MESSAGE)));
+            storedMessage.add(cursor.getString(cursor.getColumnIndex(KEY_MESSAGE)));
+            cursor.moveToNext();
+        }
+        Log.i(ACTIVITY_NAME,"Cursor's column count = " + cursor.getColumnCount());
+
+        cursor.moveToFirst();
+        for(int i=0;i<numResults;i++){
+            Log.i(ACTIVITY_NAME,"The " + i + " row is " + cursor.getString(cursor.getColumnIndex(KEY_MESSAGE)));
+            cursor.moveToNext();
+        }
     }
 
     private class ChatAdapter extends ArrayAdapter<String>{
@@ -69,5 +113,31 @@ public class ChatWindow extends Activity {
             message.setText(getItem(position));
             return result;
         }
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME,"In onResume()");
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME,"In onStart()");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME,"In onPause()");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME,"In onStop()");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        db.close();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
     }
 }
